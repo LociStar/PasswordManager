1288221cfd29dd6a93c59f35d64dc8d5ac2c9765
LociStar <locistar.got@gmail.com>
2021-03-09 17:50:52 +0100

    rename to GenerateMPHash

--- FXPM/src/main/java/com/passswordmanager/TestMain/GenerateMPHash.java
+++ FXPM/src/main/java/com/passswordmanager/TestMain/GenerateMPHash.java
@@ -5,20 +5,22 @@ import com.lambdaworks.crypto.SCryptUtil;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-public class encryptStart {
+public class GenerateMPHash {
     public static void main(String[] args) {
 
+        System.out.print("Master Password: ");
         Scanner s = new Scanner(System.in);
         String password = s.nextLine();
         s.close();
 
         String hash = SCryptUtil.scrypt(password, 8, 8, 8);
+        System.out.println("Hash:");
         System.out.println(hash);
 
         boolean matched = SCryptUtil.check(password, hash);
-        System.out.println(matched);
+        System.out.println("Must be True: " + matched);
 
         matched = SCryptUtil.check("passwordno", hash);
-        System.out.println(matched);
+        System.out.println("Must be false: " + matched);
     }
 }
