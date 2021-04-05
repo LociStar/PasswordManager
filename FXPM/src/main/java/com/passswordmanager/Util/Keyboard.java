c0d507b282e2441664158c00ae0ac0ffb4bb13d3
LokiStar <locistar.got@gmail.com>
2021-04-05 15:47:34 +0200

    java language level set to 14

--- FXPM/src/main/java/com/passswordmanager/Util/Keyboard.java
+++ FXPM/src/main/java/com/passswordmanager/Util/Keyboard.java
@@ -231,310 +231,112 @@ public class Keyboard {
      */
     public void type(char character) {
         switch (character) {
-            case 'a':
-                doType(KeyEvent.VK_A);
-                break;
-            case 'b':
-                doType(KeyEvent.VK_B);
-                break;
-            case 'c':
-                doType(KeyEvent.VK_C);
-                break;
-            case 'd':
-                doType(KeyEvent.VK_D);
-                break;
-            case 'e':
-                doType(KeyEvent.VK_E);
-                break;
-            case 'f':
-                doType(KeyEvent.VK_F);
-                break;
-            case 'g':
-                doType(KeyEvent.VK_G);
-                break;
-            case 'h':
-                doType(KeyEvent.VK_H);
-                break;
-            case 'i':
-                doType(KeyEvent.VK_I);
-                break;
-            case 'j':
-                doType(KeyEvent.VK_J);
-                break;
-            case 'k':
-                doType(KeyEvent.VK_K);
-                break;
-            case 'l':
-                doType(KeyEvent.VK_L);
-                break;
-            case 'm':
-                doType(KeyEvent.VK_M);
-                break;
-            case 'n':
-                doType(KeyEvent.VK_N);
-                break;
-            case 'o':
-                doType(KeyEvent.VK_O);
-                break;
-            case 'p':
-                doType(KeyEvent.VK_P);
-                break;
-            case 'q':
-                doType(KeyEvent.VK_Q);
-                break;
-            case 'r':
-                doType(KeyEvent.VK_R);
-                break;
-            case 's':
-                doType(KeyEvent.VK_S);
-                break;
-            case 't':
-                doType(KeyEvent.VK_T);
-                break;
-            case 'u':
-                doType(KeyEvent.VK_U);
-                break;
-            case 'v':
-                doType(KeyEvent.VK_V);
-                break;
-            case 'w':
-                doType(KeyEvent.VK_W);
-                break;
-            case 'x':
-                doType(KeyEvent.VK_X);
-                break;
-            case 'y':
-                doType(KeyEvent.VK_Y);
-                break;
-            case 'z':
-                doType(KeyEvent.VK_Z);
-                break;
-            case 'A':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_A);
-                break;
-            case 'B':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_B);
-                break;
-            case 'C':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_C);
-                break;
-            case 'D':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_D);
-                break;
-            case 'E':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_E);
-                break;
-            case 'F':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_F);
-                break;
-            case 'G':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_G);
-                break;
-            case 'H':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_H);
-                break;
-            case 'I':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_I);
-                break;
-            case 'J':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_J);
-                break;
-            case 'K':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_K);
-                break;
-            case 'L':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_L);
-                break;
-            case 'M':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_M);
-                break;
-            case 'N':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_N);
-                break;
-            case 'O':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_O);
-                break;
-            case 'P':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_P);
-                break;
-            case 'Q':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Q);
-                break;
-            case 'R':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_R);
-                break;
-            case 'S':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_S);
-                break;
-            case 'T':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_T);
-                break;
-            case 'U':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_U);
-                break;
-            case 'V':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_V);
-                break;
-            case 'W':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_W);
-                break;
-            case 'X':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_X);
-                break;
-            case 'Y':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Y);
-                break;
-            case 'Z':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Z);
-                break;
-            case 'ß':
-                doTypeUnicode('ß');
-                break;
-            case '`':
-                doType(KeyEvent.VK_BACK_QUOTE);
-                break;
-            case '0':
-                doType(KeyEvent.VK_0);
-                break;
-            case '1':
-                doType(KeyEvent.VK_1);
-                break;
-            case '2':
-                doType(KeyEvent.VK_2);
-                break;
-            case '3':
-                doType(KeyEvent.VK_3);
-                break;
-            case '4':
-                doType(KeyEvent.VK_4);
-                break;
-            case '5':
-                doType(KeyEvent.VK_5);
-                break;
-            case '6':
-                doType(KeyEvent.VK_6);
-                break;
-            case '7':
-                doType(KeyEvent.VK_7);
-                break;
-            case '8':
-                doType(KeyEvent.VK_8);
-                break;
-            case '9':
-                doType(KeyEvent.VK_9);
-                break;
-            case '-':
-                doType(KeyEvent.VK_MINUS);
-                break;
-            case '=':
-                doType(KeyEvent.VK_EQUALS);
-                break;
-            case '~':
-                doTypeUnicode('~');
-                break;
-            case '!':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_1);
-                break;
-            case '@':
-                doTypeUnicode('@');
-                break;
-            case '#':
-                doType(KeyEvent.VK_NUMBER_SIGN);
-                break;
-            case '$':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_4);
-                break;
-            case '%':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_5);
-                break;
-            case '^':
-                doTypeUnicode('^');
-                break;
-            case '&':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_6);
-                break;
-            case '*':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PLUS);
-                break;
-            case '(':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_8);
-                break;
-            case ')':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_9);
-                break;
-            case '_':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_MINUS);
-                break;
-            case '+':
-                doType(KeyEvent.VK_PLUS);
-                break;
-            case '\t':
-                doType(KeyEvent.VK_TAB);
-                break;
-            case '\n':
-                doType(KeyEvent.VK_ENTER);
-                break;
-            case '[':
-                doType(KeyEvent.VK_OPEN_BRACKET);
-                break;
-            case ']':
-                doType(KeyEvent.VK_CLOSE_BRACKET);
-                break;
-            case '\\':
-                doType(KeyEvent.VK_ALT_GRAPH, KeyEvent.VK_7);
-                break;
-            case '{':
-                doTypeUnicode('{');
-                break;
-            case '}':
-                doTypeUnicode('}');
-                break;
-            case '|':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_BACK_SLASH);
-                break;
-            case ';':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_COMMA);
-                break;
-            case ':':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PERIOD);
-                break;
-            case '\'':
-                doTypeUnicode('\'');
-                break;
-            case '"':
-                doType(KeyEvent.VK_QUOTEDBL);
-                break;
-            case ',':
-                doType(KeyEvent.VK_COMMA);
-                break;
-            case '<':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_COMMA);
-                break;
-            case '.':
-                doType(KeyEvent.VK_PERIOD);
-                break;
-            case '>':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PERIOD);
-                break;
-            case '/':
-                doType(KeyEvent.VK_SLASH);
-                break;
-            case '?':
-                doTypeUnicode('?');
-                break;
-            case ' ':
-                doType(KeyEvent.VK_SPACE);
-                break;
-            case '§':
-                doType(KeyEvent.VK_SHIFT, KeyEvent.VK_3);
-                break;
-            default:
+            case 'a' -> doType(KeyEvent.VK_A);
+            case 'b' -> doType(KeyEvent.VK_B);
+            case 'c' -> doType(KeyEvent.VK_C);
+            case 'd' -> doType(KeyEvent.VK_D);
+            case 'e' -> doType(KeyEvent.VK_E);
+            case 'f' -> doType(KeyEvent.VK_F);
+            case 'g' -> doType(KeyEvent.VK_G);
+            case 'h' -> doType(KeyEvent.VK_H);
+            case 'i' -> doType(KeyEvent.VK_I);
+            case 'j' -> doType(KeyEvent.VK_J);
+            case 'k' -> doType(KeyEvent.VK_K);
+            case 'l' -> doType(KeyEvent.VK_L);
+            case 'm' -> doType(KeyEvent.VK_M);
+            case 'n' -> doType(KeyEvent.VK_N);
+            case 'o' -> doType(KeyEvent.VK_O);
+            case 'p' -> doType(KeyEvent.VK_P);
+            case 'q' -> doType(KeyEvent.VK_Q);
+            case 'r' -> doType(KeyEvent.VK_R);
+            case 's' -> doType(KeyEvent.VK_S);
+            case 't' -> doType(KeyEvent.VK_T);
+            case 'u' -> doType(KeyEvent.VK_U);
+            case 'v' -> doType(KeyEvent.VK_V);
+            case 'w' -> doType(KeyEvent.VK_W);
+            case 'x' -> doType(KeyEvent.VK_X);
+            case 'y' -> doType(KeyEvent.VK_Y);
+            case 'z' -> doType(KeyEvent.VK_Z);
+            case 'A' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_A);
+            case 'B' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_B);
+            case 'C' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_C);
+            case 'D' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_D);
+            case 'E' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_E);
+            case 'F' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_F);
+            case 'G' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_G);
+            case 'H' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_H);
+            case 'I' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_I);
+            case 'J' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_J);
+            case 'K' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_K);
+            case 'L' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_L);
+            case 'M' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_M);
+            case 'N' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_N);
+            case 'O' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_O);
+            case 'P' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_P);
+            case 'Q' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Q);
+            case 'R' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_R);
+            case 'S' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_S);
+            case 'T' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_T);
+            case 'U' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_U);
+            case 'V' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_V);
+            case 'W' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_W);
+            case 'X' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_X);
+            case 'Y' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Y);
+            case 'Z' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_Z);
+            case 'ß' -> doTypeUnicode('ß');
+            case '`' -> doType(KeyEvent.VK_BACK_QUOTE);
+            case '0' -> doType(KeyEvent.VK_0);
+            case '1' -> doType(KeyEvent.VK_1);
+            case '2' -> doType(KeyEvent.VK_2);
+            case '3' -> doType(KeyEvent.VK_3);
+            case '4' -> doType(KeyEvent.VK_4);
+            case '5' -> doType(KeyEvent.VK_5);
+            case '6' -> doType(KeyEvent.VK_6);
+            case '7' -> doType(KeyEvent.VK_7);
+            case '8' -> doType(KeyEvent.VK_8);
+            case '9' -> doType(KeyEvent.VK_9);
+            case '-' -> doType(KeyEvent.VK_MINUS);
+            case '=' -> doType(KeyEvent.VK_EQUALS);
+            case '~' -> doTypeUnicode('~');
+            case '!' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_1);
+            case '@' -> doTypeUnicode('@');
+            case '#' -> doType(KeyEvent.VK_NUMBER_SIGN);
+            case '$' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_4);
+            case '%' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_5);
+            case '^' -> doTypeUnicode('^');
+            case '&' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_6);
+            case '*' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PLUS);
+            case '(' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_8);
+            case ')' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_9);
+            case '_' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_MINUS);
+            case '+' -> doType(KeyEvent.VK_PLUS);
+            case '\t' -> doType(KeyEvent.VK_TAB);
+            case '\n' -> doType(KeyEvent.VK_ENTER);
+            case '[' -> doType(KeyEvent.VK_OPEN_BRACKET);
+            case ']' -> doType(KeyEvent.VK_CLOSE_BRACKET);
+            case '\\' -> doType(KeyEvent.VK_ALT_GRAPH, KeyEvent.VK_7);
+            case '{' -> doTypeUnicode('{');
+            case '}' -> doTypeUnicode('}');
+            case '|' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_BACK_SLASH);
+            case ';' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_COMMA);
+            case ':' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PERIOD);
+            case '\'' -> doTypeUnicode('\'');
+            case '"' -> doType(KeyEvent.VK_QUOTEDBL);
+            case ',' -> doType(KeyEvent.VK_COMMA);
+            case '<' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_COMMA);
+            case '.' -> doType(KeyEvent.VK_PERIOD);
+            case '>' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_PERIOD);
+            case '/' -> doType(KeyEvent.VK_SLASH);
+            case '?' -> doTypeUnicode('?');
+            case ' ' -> doType(KeyEvent.VK_SPACE);
+            case '§' -> doType(KeyEvent.VK_SHIFT, KeyEvent.VK_3);
+            default -> {
                 try {
                     robot.keyPress(KeyEvent.getExtendedKeyCodeForChar(character));
                 } catch (Exception e) {
                     throw new IllegalArgumentException("Cannot type character " + character);
                 }
-
+            }
         }
     }
 
